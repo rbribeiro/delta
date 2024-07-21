124bc071f7945bb9b098d863ebb31eb27a2f2f36
rbribeiro <rb.ribeiro9@gmail.com>
2024-07-21 01:02:51 -0600

    Classes for theorem environment

--- components/progressBar.js
+++ components/progressBar.js
@@ -1,44 +1,42 @@
 class ProgressBar extends HTMLElement {
-	constructor() {
-		super();
-        const link = document.getElementsByTagName('link')[0].cloneNode()
-		const shadow = this.attachShadow({ mode: "open" });
-        shadow.appendChild(link)
+  constructor() {
+    super();
+    const link = document.getElementsByTagName("link")[0].cloneNode();
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(link);
 
-		const barContainer = document.createElement("div");
-		barContainer.classList.add("pbContainer");
+    const barContainer = document.createElement("div");
+    barContainer.classList.add("pbContainer");
 
-		const bar = document.createElement("div");
-        bar.classList.add('pb')
-		bar.style.width = "0";
-		bar.style.height = "100%";
+    const bar = document.createElement("div");
+    bar.classList.add("pb");
+    bar.style.width = "0";
+    bar.style.height = "100%";
 
-		barContainer.appendChild(bar);
-		shadow.appendChild(barContainer);
+    barContainer.appendChild(bar);
+    shadow.appendChild(barContainer);
 
-		this.bar = bar;
-	}
+    this.bar = bar;
+  }
 
-	connectedCallback() {
-		// Listen for the custom event
-		document.addEventListener(
-			"appStateChange",
-			this.handleCustomEvent.bind(this)
-		);
-	}
+  connectedCallback() {
+    // Listen for the custom event
+    document.addEventListener(
+      "appStateChange",
+      this.handleCustomEvent.bind(this),
+    );
+  }
 
-	handleCustomEvent(event) {
-		console.log("Custom event received:", event.detail);
-		console.log(this);
-		this.bar.style.width = `${
-			(100 * event.detail.currentSlide) / event.detail.totalSlides
-		}%`;
-	}
+  handleCustomEvent(event) {
+    this.bar.style.width = `${
+      (100 * event.detail.currentSlide) / event.detail.totalSlides
+    }%`;
+  }
 
-	updateProgress(current, total) {
-		const progress = (current / total) * 100;
-		this.innerHTML = `${progress}%`;
-	}
+  updateProgress(current, total) {
+    const progress = (current / total) * 100;
+    this.innerHTML = `${progress}%`;
+  }
 }
 
 customElements.define("progress-bar", ProgressBar);
