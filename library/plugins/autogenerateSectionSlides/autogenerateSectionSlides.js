1cf02565171d718e5eb2485cae815c9c52011aaa
rbribeiro <rb.ribeiro9@gmail.com>
2024-07-26 21:40:04 -0600

    Fixed bug with section slide generator

--- library/plugins/autogenerateSectionSlides/autogenerateSectionSlides.js
+++ library/plugins/autogenerateSectionSlides/autogenerateSectionSlides.js
@@ -1,31 +1,31 @@
-const autogenerateSectionSlides = {}
+const autogenerateSectionSlides = {};
 
 autogenerateSectionSlides.init = function () {
-    const sections = document.querySelectorAll("section") || [];
+  const sections = document.querySelectorAll("section") || [];
 
-	sections.forEach((section,key) => {
-		autogenerateSectionSlides.sectionBuilder(section,key+1);
-	});
-
-}
-
-autogenerateSectionSlides.sectionBuilder = function (section, number) {
-	if (
-		section.children &&
-		section.children[0].tagName.toLowerCase() == "title"
-	) {
-		const titleSlide = document.createElement("slide");
-		titleSlide.classList.add("section-intro");
-		let sectionTitle = section.children[0].innerHTML;
-		const textArea = document.createElement("textarea");
-		textArea.innerHTML = sectionTitle;
-		sectionTitle = textArea.value;
-		titleSlide.innerHTML = `<h1>${sectionTitle}</h1>
-								<hr />`;
-		if (number) section.setAttribute("id", `DELTA_SECTION_${number}`);
-		section.removeChild(section.children[0]);
-		section.prepend(titleSlide);
-	}
+  sections.forEach((section, key) => {
+    autogenerateSectionSlides.sectionBuilder(section, key + 1);
+  });
 };
 
-autogenerateSectionSlides.init()
+autogenerateSectionSlides.sectionBuilder = function (section, number) {
+  if (
+    section.children &&
+    section.children[0].tagName.toLowerCase() == "title"
+  ) {
+    const titleSlide = document.createElement("slide");
+    titleSlide.classList.add("section-intro");
+    let sectionTitle = section.children[0].innerHTML;
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = sectionTitle;
+    sectionTitle = textArea.value;
+    section.setAttribute("data-title", sectionTitle);
+    titleSlide.innerHTML = `<h1>${sectionTitle}</h1>
+								<hr />`;
+    if (number) section.setAttribute("id", `DELTA_SECTION_${number}`);
+    section.removeChild(section.children[0]);
+    section.prepend(titleSlide);
+  }
+};
+
+autogenerateSectionSlides.init();
--- library/plugins/tableOfContents/tableOfContents.js
+++ library/plugins/tableOfContents/tableOfContents.js
@@ -1,44 +1,47 @@
 class tableOfContents extends HTMLElement {
-	constructor() {
-		super();
-		const ol = document.createElement("ol");
-		this.ol = ol;
-		this.appendChild(ol);
-		
+  constructor() {
+    super();
+    const ol = document.createElement("ol");
+    this.ol = ol;
+    this.appendChild(ol);
+  }
 
-	}
+  connectedCallback() {
+    const titles = [];
+    const sections = document.querySelectorAll("section") || [];
+    sections.forEach((section) => {
+      const dataTitle = section.getAttribute("data-title");
+      if (dataTitle) {
+        titles.push(dataTitle);
+      } else if (
+        section.children.length > 0 &&
+        section.children[0].tagName.toLocaleLowerCase() == "title"
+      ) {
+        let sectionTitle = section.children[0].innerHTML;
+        const textArea = document.createElement("textarea");
+        textArea.innerHTML = sectionTitle;
+        sectionTitle = textArea.value;
+        titles.push(sectionTitle);
+      }
+    });
 
-	connectedCallback() {
-		const titles = [];
-		const sections = document.querySelectorAll("section") || [];
+    titles.forEach((title, key) => {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      if (this.hasAttribute("animate")) li.classList.add("step");
+      const sectionId = `DELTA_SECTION_${key + 1}`;
+      a.addEventListener("click", () => {
+        this.handleClick(sectionId);
+      });
+      a.innerHTML = title;
+      li.appendChild(a);
+      this.ol.appendChild(li);
+    });
+  }
 
-		sections.forEach((section) => {
-			if (section.children.length > 0 && section.children[0].tagName.toLocaleLowerCase() == "title") {
-                let sectionTitle = section.children[0].innerHTML;
-                const textArea = document.createElement("textarea")
-                textArea.innerHTML = sectionTitle
-                sectionTitle = textArea.value
-				titles.push(sectionTitle);
-			}
-		});
-
-		titles.forEach((title,key) => {
-			const li = document.createElement("li");
-			const a = document.createElement("a")
-			if(this.hasAttribute("animate")) li.classList.add("step")
-            const sectionId = `DELTA_SECTION_${key+1}`
-			a.addEventListener("click", () => {
-				this.handleClick(sectionId)
-			})
-			a.innerHTML = title
-			li.appendChild(a)
-			this.ol.appendChild(li);
-		});
-	}	
-
-	handleClick (id) {
-		Delta.goToElementById(id)
-	}
+  handleClick(id) {
+    Delta.goToElementById(id);
+  }
 }
 
 customElements.define("table-contents", tableOfContents);
