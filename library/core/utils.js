7720a20b9295107058471fe7ae17ea0534b43a02
rbribeiro <rb.ribeiro9@gmail.com>
2024-08-16 14:50:02 -0600

    Added getElement class

--- library/core/utils.js
+++ library/core/utils.js
@@ -1,21 +1,68 @@
 Node.prototype.findParentByTagName = function (tagName) {
-  let currentElement = this.parentElement;
-  while (currentElement) {
-    if (currentElement.tagName.toLowerCase() === tagName.toLowerCase()) {
-      return currentElement;
-    }
-    currentElement = currentElement.parentElement;
-  }
-  return null;
+	let currentElement = this.parentElement;
+	while (currentElement) {
+		if (currentElement.tagName.toLowerCase() === tagName.toLowerCase()) {
+			return currentElement;
+		}
+		currentElement = currentElement.parentElement;
+	}
+	return null;
 };
 
 Node.prototype.findParentByClass = function (className) {
-  let currentElement = this.parentElement;
-  while (currentElement) {
-    if (currentElement.classList.contains(className)) {
-      return currentElement;
-    }
-    currentElement = currentElement.parentElement;
-  }
-  return null;
+	let currentElement = this.parentElement;
+	while (currentElement) {
+		if (currentElement.classList.contains(className)) {
+			return currentElement;
+		}
+		currentElement = currentElement.parentElement;
+	}
+	return null;
 };
+
+class GetElement extends HTMLElement {
+	constructor() {
+		super();
+	}
+
+	observedAttributes() {
+		return ["target"];
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		if (name == "target") {
+			this.render();
+		}
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		this.innerHTML = "";
+		const targetId = this.target;
+		const target = document.getElementById(targetId);
+
+		if (target) {
+			this.innerHTML = target.innerHTML;
+		}
+	}
+
+	observeTarget(id) {
+		if (this.observer) {
+			this.observer.disconnect();
+		}
+
+		const target = document.getElementById(id);
+    if(target) {
+      this.observer = new MutationObserver(() => this.render())
+      this.observer.observe(target, {attributes : true, childList : true})
+      this.render()
+    }
+	}
+}
+
+Delta.getInstance().eventDispatcher.on("deltaIsReady", () => {
+  customElements.define("get-elem", GetElement)
+})
