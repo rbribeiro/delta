dad31a75ce6641f19e8dff436a3cc6cf460e5ac7
rbribeiro <rb.ribeiro9@gmail.com>
2024-08-29 12:03:31 -0600

    stuff

--- edge-step.js
+++ edge-step.js
@@ -41,7 +41,68 @@ edgeStep.kbGraph = function () {
       padding: 10,
     },
   });
-  console.log("oi");
 };
 
-Delta.getInstance().eventDispatcher.on("deltaIsReady", edgeStep.kbGraph());
+edgeStep.justAGraph = function() {
+  const graph = cytoscape({
+    container: document.getElementById('graphExample'), // container to render in
+
+    elements: [ // list of graph elements to start with
+      { data: { id: 'a' } },
+      { data: { id: 'b' } },
+      { data: { id: 'c' } },
+      { data: { id: 'd' } },
+      { data: { id: 'e' } },
+      { data: { id: 'f' } },
+      { data: { id: 'g' } },
+      { data: { id: 'h' } },
+      { data: { id: 'i' } },
+      { data: { id: 'j' } },
+      
+      { data: { id: 'ab', source: 'a', target: 'b' } },
+      { data: { id: 'ac', source: 'a', target: 'c' } },
+      { data: { id: 'bd', source: 'b', target: 'd' } },
+      { data: { id: 'ce', source: 'c', target: 'e' } },
+      { data: { id: 'df', source: 'd', target: 'f' } },
+      { data: { id: 'eg', source: 'e', target: 'g' } },
+      { data: { id: 'fh', source: 'f', target: 'h' } },
+      { data: { id: 'gi', source: 'g', target: 'i' } },
+      { data: { id: 'hj', source: 'h', target: 'j' } },
+      { data: { id: 'ia', source: 'i', target: 'a' } },
+      { data: { id: 'jb', source: 'j', target: 'b' } },
+      { data: { id: 'fc', source: 'f', target: 'c' } },
+    ],
+
+    style: [ // the stylesheet for the graph
+      {
+        selector: 'node',
+        style: {
+          'background-color': '#ed6a5a',
+          'label': 'data(id)',
+        }
+      },
+
+      {
+        selector: 'edge',
+        style: {
+          'width': 2,
+          'line-color': 'green',
+          'target-arrow-color': 'green',
+          'target-arrow-shape': 'none', // No arrows
+        }
+      }
+    ],
+
+    layout: {
+      name: 'grid',
+      rows: 3
+    }
+  });
+}
+
+edgeStep.init = function() {
+  edgeStep.kbGraph()
+  edgeStep.justAGraph()
+}
+
+Delta.getInstance().eventDispatcher.on("deltaIsReady", edgeStep.init());
