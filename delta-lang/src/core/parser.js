b180485cc1fb7050a99096df8157635301339e2d
Rodrigo Ribeiro <rb.ribeiro9@gmail.com>
2025-11-14 16:37:36 -0300

    Corrigindo issue #19 onde a hierarquia de um componente dependia de linhas vazias acima dele.

--- delta-lang/src/core/parser.js
+++ delta-lang/src/core/parser.js
@@ -111,7 +111,7 @@ class Parser {
                 this.addToHierarchy({
                     type: 'paragraph',
                     children: [],
-                    indent: indent
+                    indent: this.stack[this.stack.length-1].indent
                 })
             }
         }
